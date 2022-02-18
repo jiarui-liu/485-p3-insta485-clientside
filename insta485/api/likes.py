5dc9183bf714ea45c03892e93ccbc2e47e9ff218
Jiarui Liu <ljrjerry@umich.edu>
2022-02-18 15:12:34 -0500

    debug likes

--- insta485/api/likes.py
+++ insta485/api/likes.py
@@ -29,7 +29,10 @@ def create_like():
         cur = connection.execute(
             "SELECT * FROM likes"
         ).fetchall()
-        context["likeid"] = cur[-1]["likeid"] + 1
+        if len(cur) == 0:
+            context["likeid"] = 1
+        else:
+            context["likeid"] = cur[-1]["likeid"] + 1
         context["url"] = "/api/v1/likes/" + str(context["likeid"]) + "/"
         # create one like
         connection.execute(
