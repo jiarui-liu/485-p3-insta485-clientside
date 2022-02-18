24cc90ca8b573fb3acbf238968c4b21334e7651b
zhengchenhao-solo <1157430339@qq.com>
2022-02-18 11:59:18 -0500

    finish comment creation and deletion

--- insta485/js/post.jsx
+++ insta485/js/post.jsx
@@ -43,8 +43,10 @@ class Post extends React.Component {
     // Initialize mutable state
     super(props);
     this.state = { imgUrl: '', owner: '', ownerImgUrl:'', ownerShowUrl:'', postShowUrl:'', postid: 1, created: '',
-     numlikes: 1, loglike: true, likeurl: ''};
+     numlikes: 1, loglike: true, likeurl: '', comments: []};
     this.handleLike = this.handleLike.bind(this);
+    this.deleteComment = this.deleteComment.bind(this);
+    this.createComment = this.createComment.bind(this);
   }
 
   componentDidMount() {
@@ -69,14 +71,14 @@ class Post extends React.Component {
           created: moment(data.created).fromNow(),
           numlikes: data.likes.numLikes,
           loglike: data.likes.lognameLikesThis,
-          likeurl: data.likes.url
+          likeurl: data.likes.url,
+          comments: data.comments
         });
       })
       .catch((error) => console.log(error));
   }
 
   handleLike(){
-    console.log(this.state)
     if (this.state.loglike){
       this.setState(prevState =>({
         loglike: !prevState.loglike,
@@ -90,6 +92,7 @@ class Post extends React.Component {
         .then((response) => {
           if (!response.ok) throw Error(response.statusText);
         })
+        .catch((error) => console.log(error));
     }
     else{
       this.setState(prevState =>({
@@ -111,17 +114,64 @@ class Post extends React.Component {
           this.setState({ likeurl: data.url });
           console.log(this.state.likeurl, data.likeid)
         })
+        .catch((error) => console.log(error));
     }
   }
 
+  deleteComment(commentid){
+    let newComments = this.state.comments.slice();
+    for (var i=0; i<newComments.length; i++){
+      if (newComments[i].commentid == commentid){
+        newComments.splice(i,1);
+      }
+    }
+    this.setState({ comments: newComments });
+    const requestOption = {
+      method: 'DELETE',
+      credentials: 'same-origin'
+    }
+    const url = "/api/v1/comments/" + commentid.toString() + "/";
+    fetch(url, requestOption)
+      .then((response) => {
+        if (!response.ok) throw Error(response.statusText);
+      })
+      .catch((error) => console.log(error));
+  }
+
+  createComment(inputText){
+    const requestOption = {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ text: inputText}),
+    }
+    const url = "/api/v1/comments/?postid=" + this.state.postid.toString();
+    fetch(url, requestOption)
+    .then((response) => {
+      if (!response.ok) throw Error(response.statusText);
+      return response.json();
+    })
+    .then(data => {
+      let newComments = this.state.comments.slice();
+      newComments.push(data);
+      this.setState({ comments: newComments });
+    })
+    .catch((error) => console.log(error));
+  }
+  
   render() {
     // returns HTML representing this component
     // This line automatically assigns this.state.imgUrl to the const variable imgUrl
     // and this.state.owner to the const variable owner
-    const { imgUrl, owner, ownerImgUrl, ownerShowUrl, postShowUrl, postid, created, numlikes, loglike, likeurl} = this.state;
+    const { imgUrl, owner, ownerImgUrl, ownerShowUrl, postShowUrl, postid, created, numlikes, loglike, likeurl,comments} = this.state;
     let likestr = '';
     if (numlikes === 1) likestr = '1 like';
     else likestr = numlikes.toString() + ' likes';
+
+    let commentField = comments.map( (comment) =>
+    <Comment comment={comment} deleteComment={this.deleteComment}  key={comment.url}/>
+    );
+    
     // console.log(loglike)
     return (
       <div className="myDivSubBody">
@@ -143,6 +193,8 @@ class Post extends React.Component {
           {likestr}
         </p>
         <LikeButton islike={loglike} handleLike={this.handleLike} />
+        <ul className="main">{commentField}</ul>
+        <CommentCreateForm createComment={this.createComment} />
       </div>
     );
   }
@@ -156,12 +208,68 @@ class LikeButton extends React.Component {
   }
   render(){
     return (
-      <button onClick={() => {this.props.handleLike()}}>
+      <button  className="like-unlike-button" onClick={() => {this.props.handleLike()}}>
         {this.props.islike? 'unlike':'like'}
       </button>
     );
   }
 }
+
+
+class Comment extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <li className="comment">
+      <b><a href = { this.props.comment.ownerShowUrl }>{ this.props.comment.owner }: </a></b> 
+      { this.props.comment.text }
+      {
+        this.props.comment.lognameOwnsThis && 
+        <button className="delete-comment-button" onClick={() => {this.props.deleteComment(this.props.comment.commentid)}} >
+          Delete comment
+        </button>
+      }
+      </li>
+    );
+  }
+}
+
+
+class CommentCreateForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    this.setState({value: ''});
+    this.props.createComment(this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form className="comment-form" onSubmit={this.handleSubmit}>
+        <label>
+          Create Comment: 
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+      </form>
+    );
+  }
+}
+
+
+
 
 
 
