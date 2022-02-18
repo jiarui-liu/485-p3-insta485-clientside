fbf1cb93a650a4c1cd70e1e094acd7b2e1ae6d94
zhengchenhao-solo <1157430339@qq.com>
2022-02-18 01:54:13 -0500

    finish like button

--- insta485/js/post.jsx
+++ insta485/js/post.jsx
@@ -42,7 +42,9 @@ class Post extends React.Component {
     // runs when an instance is created
     // Initialize mutable state
     super(props);
-    this.state = { imgUrl: '', owner: '', ownerImgUrl:'', ownerShowUrl:'', postShowUrl:'', postid: 1, created: '', numlikes: 1};
+    this.state = { imgUrl: '', owner: '', ownerImgUrl:'', ownerShowUrl:'', postShowUrl:'', postid: 1, created: '',
+     numlikes: 1, loglike: true, likeurl: ''};
+    this.handleLike = this.handleLike.bind(this);
   }
 
   componentDidMount() {
@@ -65,23 +67,62 @@ class Post extends React.Component {
           postShowUrl: data.postShowUrl,
           postid: data.postid,
           created: moment(data.created).fromNow(),
-          numlikes: data.likes.numLikes
+          numlikes: data.likes.numLikes,
+          loglike: data.likes.lognameLikesThis,
+          likeurl: data.likes.url
         });
       })
       .catch((error) => console.log(error));
+  }
+
+  handleLike(){
+    console.log(this.state)
+    if (this.state.loglike){
+      this.setState(prevState =>({
+        loglike: !prevState.loglike,
+        numlikes: prevState.numlikes - 1
+      }));
+      const requestOption = {
+        method: 'DELETE',
+        credentials: 'same-origin'
+      }
+      fetch(this.state.likeurl, requestOption)
+        .then((response) => {
+          if (!response.ok) throw Error(response.statusText);
+        })
+    }
+    else{
+      this.setState(prevState =>({
+        loglike: !prevState.loglike,
+        numlikes: prevState.numlikes + 1
+      }));
+      const requestOption = {
+        method: 'POST',
+        credentials: 'same-origin'
+      }
+      const url = "http://localhost:8000/api/v1/likes/?postid=" + this.state.postid.toString();
+      fetch(url, requestOption)
+        .then((response) => {
+          if (!response.ok) throw Error(response.statusText);
+          console.log(response)
+          return response.json();
+        })
+        .then(data => {
+          this.setState({ likeurl: data.url });
+          console.log(this.state.likeurl, data.likeid)
+        })
+    }
   }
 
   render() {
     // returns HTML representing this component
     // This line automatically assigns this.state.imgUrl to the const variable imgUrl
     // and this.state.owner to the const variable owner
-    const { imgUrl, owner, ownerImgUrl, ownerShowUrl, postShowUrl, postid, created, numlikes} = this.state;
+    const { imgUrl, owner, ownerImgUrl, ownerShowUrl, postShowUrl, postid, created, numlikes, loglike, likeurl} = this.state;
     let likestr = '';
     if (numlikes === 1) likestr = '1 like';
     else likestr = numlikes.toString() + ' likes';
-    console.log(imgUrl, owner, ownerImgUrl, ownerShowUrl, postShowUrl, postid, created, numlikes)
-    // Render number of post image and post owner
-    //"font-size: 20px; padding: 5px"
+    // console.log(loglike)
     return (
       <div className="myDivSubBody">
         <ul className='head'>
@@ -101,10 +142,28 @@ class Post extends React.Component {
         <p >
           {likestr}
         </p>
+        <LikeButton islike={loglike} handleLike={this.handleLike} />
       </div>
     );
   }
 }
+
+
+
+class LikeButton extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <button onClick={() => {this.props.handleLike()}}>
+        {this.props.islike? 'unlike':'like'}
+      </button>
+    );
+  }
+}
+
+
 
 Post.propTypes = {
   url: PropTypes.string.isRequired,
